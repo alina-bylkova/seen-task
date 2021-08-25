374cc94d6be9fc17ac39007af0c29730ffb62fb1
Alina Bylkova <alina.bylkova@coop.no>
2021-08-25 21:07:19 +0200

    Implemented graceful shutdown for server

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/alinabylkova/seen-task/api"
 	"github.com/alinabylkova/seen-task/config/env"
@@ -22,9 +27,9 @@ func main() {
 	}
 	db.Gorm.AutoMigrate(&model.Recipient{}, &model.Video{}, &model.Event{})
 
-	r := gin.Default()
+	router := gin.Default()
 
-	authorized := r.Group("/api", gin.BasicAuth(gin.Accounts{
+	authorized := router.Group("/api", gin.BasicAuth(gin.Accounts{
 		config.AuthUser: config.AuthPassword,
 	}))
 
@@ -32,5 +37,31 @@ func main() {
 	authorized.GET("recipients/:id", api.GetRecipientById(db))
 	authorized.POST("recipients", api.PostRecipient(db))
 
-	r.Run()
+	server := &http.Server{
+		Addr:    config.ServerAddress,
+		Handler: router,
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	go func() {
+		log.Printf("Serer is listening on port: %s", config.ServerAddress)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Listen: %s\n", err)
+		}
+	}()
+
+	<-ctx.Done()
+	stop()
+	log.Println("Shutting down gracefully")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown: ", err)
+	}
+
+	log.Println("Server exiting")
 }
